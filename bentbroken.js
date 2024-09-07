badb53f1fe2986bc4133158e0915fe12574a4ea1
bger-dhoward <dhoward@ballinger.com>
2024-09-07 12:27:04 -0400

    completed 6x6 sketches

--- bentbroken.js
+++ bentbroken.js
@@ -1,6 +1,43 @@
+function colorprob(x,col_locs) {
+  let total = 0;
+  let ex = 4;
+  for (let c of col_locs) {
+    let d = pow(1 -abs(x-c), ex);
+    total += d
+  }
+  let probs = [];
+  for (let c of col_locs) {
+    let d = pow(1- abs(x-c), ex);
+    probs.push(d/total);
+  }
+  
+  //console.log('colorprob', x, probs)
+  return probs
+}
+
+function colorbyloc(x, cols, col_locs){
+  let probs=colorprob(x,col_locs);
+  let cumulatives = [];
+  for (let i=0; i< cols.length; i++) {
+    let p = probs[i];
+    if (i == 0) {
+      cumulatives.push(p);
+    } else {
+      cumulatives.push(p + cumulatives[i-1])
+    }
+  }
+  let r = random();
+  for (let i=0; i < cols.length; i++) {
+    let pr = cumulatives[i];
+    if (r <= pr) {
+      //console.log("pr",cols[i]);
+      return cols[i]
+    }
+  }
+}
 
 class BentBroken {
-    constructor (origin, width, height, linespacing, minbreak, end_insets, breaks) {
+    constructor (origin, width, height, linespacing, minbreak, end_insets, breaks, slant) {
         this.breaks = breaks;
         this.origin = origin;
         this.width = width;
@@ -8,12 +45,64 @@ class BentBroken {
         this.linespacing = linespacing;
         this.minbreak = minbreak;
         this.end_insets = end_insets;
+        this.colors = [color(180,180,255), color(55, 55, 240), color(0,20,150)];
+        this.color_locs = [0, 0.5, 1];
+        this.slant = slant;
         
-        this.addBreaks();
-        this.breaks.sort();
+        if (random() < 0.3) {
+            let width_factor = map(random(), 0, 1, 0.5, 2.5);
+            this.width = this.width * width_factor;
+        }
+        
+        if (random() < 0.2) {
+            let i = floor(random(this.colors.length));
+            let newcolors = [];
+            for (let j=0; j<this.colors.length; j++) {
+                if (i==j) {
+                    newcolors.push(color('yellow'))
+                }
+                newcolors.push(this.colors[j])
+            }
+            
+            this.colors = newcolors;
+            this.color_locs = [0, 0.33, 0.66, 1];
+        }
+        
+        if (random() < 0.3) {
+            this.colors = shuffle(this.colors)
+        }
+        
+        this.maxbend = 7.5 // 0.2 about equal to 11.5 deg
+        this.bends = [];
+        
+        if (this.breaks.length < 2) {
+            this.update()
+            }
+        this.update()
+        
+        //this.addBreaks();
+        //this.addBreaks();
+        //this.breaks.sort();
     }
     
-    addBreaks () {
+    update () {
+        console.log('update')
+        if (this.breaks.length > 1 / this.minbreak * 0.6) {
+            this.removeBreaks()
+        } else if (this.breaks.length <= 2 || random() > 0.2) {
+            this.addBreaks()
+        } else {
+            this.removeBreaks() 
+        }
+        this.breaks.sort()
+    }
+    
+    addBend(x,y) {
+        let bend_angle = random(-this.maxbend, this.maxbend);
+        this.bends
+    }
+    
+    /*addBreaks () {
         let n_breaks = 2;
         let tries = 0;
         while (n_breaks > 0) {
@@ -23,18 +112,43 @@ class BentBroken {
                 n_breaks -=1;
             } else if (tries > 3){
                 let pop_i = floor(random(0,this.breaks.length));
-                console.log("breaks:", this.breaks);
-                console.log("pop_i:", pop_i);
+                //console.log("breaks:", this.breaks);
+                //console.log("pop_i:", pop_i);
                 let spliced = this.breaks.splice(pop_i);
-                console.log("popped:", spliced)
-                console.log();
+                //console.log("popped:", spliced)
+                //console.log();
                 tries = 0;
             } else {
                 tries ++;
             }
         }
+        this.breaks.sort()
+        
+    };*/
+    
+    addBreaks () {
+        console.log('addBreaks')
+        let n_breaks = 2;
+        while (n_breaks > 0) {
+            let proposed_pt = random();
+            if (this.evaluateBreak(proposed_pt)) {
+                this.breaks.push(proposed_pt)
+                n_breaks -=1;
+            }
+        }
+        this.breaks.sort()
         
     };
+    
+    removeBreaks() {
+        console.log('removeBreaks')
+        let removed = []
+        for (let n =0; n< 2; n++) {
+            let i = floor(random(0, this.breaks.length));
+            removed.push(this.breaks.splice(i,1));
+        }
+        console.log('removed: ', removed)
+    }
     
     evaluateBreak (breakpt) {
         for (let pt of this.breaks) {
@@ -48,19 +162,45 @@ class BentBroken {
     show() {
         let current_break_i = 0;
         let drawing_now = false;
-        for (let y = this.end_insets; y < height - this.end_insets; y += this.linespacing) {
+        let current_slant = false;
+        for (let y = -50; y < height + 50; y += this.linespacing) {
             let current_break = this.breaks[current_break_i];
-            //console.log("")
-            //console.log(current_break, 0,1, this.end_insets, this.height - this.end_insets)
             let break_y = map(current_break, 0,1, this.end_insets, this.height - this.end_insets);
-            //console.log("y:", y,"break_y:", break_y, "drawing now:", drawing_now)
             if (y >= break_y) {
-                //console.log('SWITCH DRAWING NOW');
                 drawing_now = ! drawing_now;
-            current_break_i ++;}
+                if (this.slant) {
+                    if (drawing_now && random() < 0.5) {
+                        current_slant = true
+                    } else {
+                        current_slant = false
+                    }
+                }
+                current_break_i ++;}
             if (drawing_now) {
-                //console.log('DRAW!',this.origin - (this.width/2), y, this.origin + (this.width/2), y)
-                line(this.origin - (this.width/2), y, this.origin + (this.width/2), y)
+                if (y > 0 && y < height && random() < 0.005) {
+                    //let s = random([1,-1,])// sign of the angle
+                    let theta;
+                    if (this.slant) {
+                       theta = 0;
+                    } else {
+                        theta = random(-this.maxbend, this.maxbend)
+                    }
+                    translate(this.origin, y);
+                    rotate(theta);
+                    translate(-this.origin, -y);
+                }
+                //let c = random([color('red'), color('blue'), color('green')]);
+                //stroke(c)
+                let yi = map(y, 0, height, 0, 1)
+                let y2 = y;
+                if (current_slant) {
+                    y2 = y + this.width * 0.8
+                } else {
+                    y2 = y
+                }
+                let c = colorbyloc(yi, this.colors, this.color_locs);
+                stroke(c)
+                line(this.origin, y, this.origin + this.width, y2)
             }
             
         }
--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 let bb;
 console.log('go!')
 
+let color_layers = [];
+
+
 function setup() {
     createCanvas(900, 900, SVG);
-    background(255)
+    background(255);
+    angleMode(DEGREES);
 }
 
 function draw() {
+    /*let colors = [color('red'), color('blue'), color('green'), color('cyan')]
+    
+    for (let i=0; i<colors.length; i++) {
+        let pts = [];
+        for (let j=0;j<200;j++) {
+            let thispt = {x1: random(width),
+                            y1: random(height),
+                            x2: random(width),
+                            y2: random(height)
+            }
+            pts.push(thispt)
+        }
+        color_layers.push(pts)
+    }
+    
+    for (let i=0;i<colors.length; i++) {
+        stroke(colors[i]);
+        for (let thispt of color_layers[i]) {
+            line(thispt.x1, thispt.y1, thispt.x2, thispt.y2)
+        }
+    }*/
+    
     stroke(0);
-    strokeWeight(1);
+    strokeWeight(2);
     //line(25,25,50,50);
     let originx = 40;
     let bb_width = 30;
     let bb_height = 900;
-    let linespacing = 3;
-    let minbreak = 0.05;
+    let linespacing = 5;
+    let minbreak = 0.03;
     let end_insets = 10;
     
     let lines = [];
     let last_breaks = [0,1];
+    let slant = random([true, false]);
     
     let nextline; // = new BentBroken(originx, bb_width, bb_height, linespacing, minbreak, end_insets, last_breaks);
     //nextline.show()
     
-    for (let x = originx; x < width - originx; x+= bb_width + 10){
+    for (let x = originx; x < width - originx; x + 10){
         console.log(x)
-        let nextline = new BentBroken(x, bb_width, bb_height, linespacing, minbreak, end_insets, last_breaks);
-        last_breaks = nextline.breaks;
+        let nextline = new BentBroken(x, bb_width, bb_height, linespacing, minbreak, end_insets, last_breaks, slant);
+        last_breaks = [...nextline.breaks];
         nextline.show();
+        lines.push(nextline);
+        x += nextline.width + 4;
     }
     //bb = new BentBroken(50, 40, 900, 2, 0.1, 10, [0,1]);
     //console.log('bb');
     //bb.show()
     
     noLoop();
+    console.log(lines)
 }
